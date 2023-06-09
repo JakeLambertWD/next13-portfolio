25b3e0617a99d2d2cb066176c7e17ca456f9b3e9
Jakelambert1 <jake.lambert@anmut.co.uk>
2023-06-09 20:47:29 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,46 @@
+import About from "@/components/About";
+import ContactMe from "@/components/ContactMe";
 import Header from "@/components/Header";
-import { getSocials } from "@/sanity/sanity-utils";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import WorkExperience from "@/components/WorkExperience";
+import { getPageinfo, getSocials, getExperience, getSkill, getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
   const socials = await getSocials();
+  const pageInfo = await getPageinfo();
+  const experiences = await getExperience();
+  const skills = await getSkill();
+  const projects = await getProjects();
 
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory scrollbar-none overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin scrollbar">
       <Header socials={socials} />
 
-      {/* <section id="hero" className="snap-start">
+      <section id="hero" className="snap-start">
         <Hero pageInfo={pageInfo} />
-      </section> */}
+      </section>
 
-      {/* <section id="experience" className="snap-center">
+      <section id="experience" className="snap-center">
         <WorkExperience experiences={experiences} />
-      </section> */}
+      </section>
 
-      {/* <section id="skills" className="snap-center">
+      <section id="skills" className="snap-center">
         <Skills technologies={skills} />
-      </section> */}
+      </section>
 
-      {/* <section id="projects" className="snap-center">
+      <section id="projects" className="snap-center">
         <Projects personalProjects={projects} />
-      </section> */}
+      </section>
 
-      {/* <section id="about" className="snap-center">
+      <section id="about" className="snap-center">
         <About pageInfo={pageInfo} />
-      </section> */}
+      </section>
 
-      {/* <section id="contact" className="snap-center">
+      <section id="contact" className="snap-center">
         <ContactMe />
-      </section> */}
+      </section>
 
       {/* <Link href="#hero">
         <footer className="sticky w-full cursor-pointer bottom-7 bottom-5 tooltip" data-tip="Back to Top">
