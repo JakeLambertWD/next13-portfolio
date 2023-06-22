336f06b754c642c2517b971ed61d63c7e1dde514
Jakelambert1 <jake.lambert@anmut.co.uk>
2023-06-22 21:17:36 +0100

    Skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,16 +21,16 @@ export default function Skill({ directionBottom, tech }: Props) {
         whileInView={{ y: 0, opacity: 1 }}
         src={urlFor(tech?.image).url()}
         className={clsx(
-          "object-contain w-20 h-20 filter transition duration-100 ease-in rounded-lg",
+          "object-contain w-14 h-14 filter transition duration-100 ease-in rounded-lg",
           "md:h-24 md:w-24",
           "xl:w-28 xl:h-28",
           "group-hover:grayscale group-hover:blur-sm"
         )}
       />
 
-      <div className="absolute z-0 w-24 h-24 transition duration-100 ease-in-out rounded-lg opacity-0 group-hover:opacity-80 group-hover:bg-white md:w-28 md:h-28 xl:w-32 xl:h-32">
+      <div className="absolute z-0 w-14 h-14 transition duration-100 ease-in-out rounded-lg opacity-0 group-hover:opacity-80 group-hover:bg-white md:w-28 md:h-28 xl:w-32 xl:h-32">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">{tech?.progress}%</p>
+          <p className="text-xl sm:3xl font-bold text-black opacity-100">{tech?.progress}%</p>
         </div>
       </div>
     </div>
