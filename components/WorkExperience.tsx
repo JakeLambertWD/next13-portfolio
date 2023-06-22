2cec60883e7597e9be5b31eb7a4c040e5b19f91f
Jakelambert1 <jake.lambert@anmut.co.uk>
2023-06-22 20:21:36 +0100

    WE

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -10,7 +10,7 @@ type Props = { experiences: Experience[] };
 
 export default function WorkExperience({ experiences }: Props) {
   return (
-    <div className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-left md:flex-row justify-evenly">
+    <div className="relative flex flex-col items-center justify-evenly h-screen max-w-full px-1 md:px-10 mx-auto overflow-hidden text-left md:flex-row">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Experience</h3>
 
       <div className="scrollbar-thin w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
