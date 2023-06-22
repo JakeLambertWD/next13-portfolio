521dd1346ee2a73663221df393c9a5448a88d447
Jakelambert1 <jake.lambert@anmut.co.uk>
2023-06-22 20:20:59 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
         <ContactMe />
       </section>
 
-      <BackToTop pageInfo={pageInfo} />
+      {/* <BackToTop pageInfo={pageInfo} /> */}
     </div>
   );
 }
