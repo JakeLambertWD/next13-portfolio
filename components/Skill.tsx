b164ce64f872aee1a86185d18b0495527d90fccf
JakeLAmbertWD <jakelambert1@hotmail.co.uk>
2024-12-26 16:46:04 +0000

    Skills styling

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -14,12 +14,16 @@ export default function Skill({ directionBottom, tech }: Props) {
   // TODO sort icons into a rainbow
 
   return (
-    <div className="relative flex cursor-pointer group tooltip" data-tip={tech?.title}>
+    <div
+      className="relative flex cursor-pointer group tooltip"
+      data-tip={tech?.title}
+    >
       <img
         src={urlFor(tech?.image).url()}
         className={clsx(
           "object-contain w-14 h-14 filter transition duration-100 ease-in rounded-lg",
           "md:h-24 md:w-24",
+          "lg:h-26 lg:w-26",
           "xl:w-28 xl:h-28",
           "group-hover:grayscale group-hover:blur-sm"
         )}
@@ -27,7 +31,9 @@ export default function Skill({ directionBottom, tech }: Props) {
 
       <div className="absolute z-0 w-14 h-14 transition duration-100 ease-in-out rounded-lg opacity-0 group-hover:opacity-80 group-hover:bg-white md:w-28 md:h-28 xl:w-32 xl:h-32">
         <div className="flex items-center justify-center h-full">
-          <p className="text-xl sm:3xl font-bold text-black opacity-100">{tech?.progress}%</p>
+          <p className="text-xl sm:3xl font-bold text-black opacity-100">
+            {tech?.progress}%
+          </p>
         </div>
       </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-const { motion } = require("framer-motion");
-
 import { Skill as SkillType } from "../typings";
 import Skill from "./Skill";
 
@@ -20,19 +18,23 @@ export default function Skills({ technologies }: Props) {
 
   return (
     <div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">My Skills</h3>
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        My Skills
+      </h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
         Hover over a skill for current proficiency
       </h3>
 
-      <div className="grid grid-cols-4 gap-8 mt-28 md:mt-0">
+      <div className="grid grid-cols-4 md:grid-cols-6 gap-8 mt-28 md:mt-0">
         {removeUnwanted?.slice(0, removeUnwanted.length / 2).map((tech) => (
           <Skill key={tech._id} tech={tech} directionBottom />
         ))}
-        {removeUnwanted?.slice(removeUnwanted.length / 2, removeUnwanted.length).map((tech) => (
-          <Skill key={tech._id} tech={tech} />
-        ))}
+        {removeUnwanted
+          ?.slice(removeUnwanted.length / 2, removeUnwanted.length)
+          .map((tech) => (
+            <Skill key={tech._id} tech={tech} />
+          ))}
       </div>
     </div>
   );
