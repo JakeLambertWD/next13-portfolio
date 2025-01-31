7c68f18c68ec9d24d5bcf4c768afb80078a05a4a
JakeLAmbertWD <jakelambert1@icloud.com>
2025-01-31 08:38:35 +0000

    Adjust project cards on a small screen

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,93 +1,65 @@
 "use client";
 
-import Link from "next/link";
-import { Card } from "react-daisyui";
 import { ExternalLink } from "react-external-link";
-
 import { Project } from "../typings";
 import Image from "next/image";
 import { urlFor } from "@/sanity/sanity-utils";
-
-const { motion } = require("framer-motion");
 
 type Props = {
   personalProjects: Project[];
 };
 export default function Projects({ personalProjects }: Props) {
-  const reverseProjects = personalProjects.slice().sort().reverse();
-
   return (
-    <div className="relative z-0 flex flex-col items-center h-screen max-w-full mx-auto overflow-hidden text-left md:flex-row justify-evenly">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+    <>
+      <h3 className="absolute w-full text-center top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
-      <div className="scrollbar p-3 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-        {reverseProjects.map((project, i) => {
-          return (
-            <div
-              key={i}
-              className="card glass w-[350px] md:w-[420px] h-[530px] md:h-[580px] mt-32 snap-center flex-shrink-0 mx-8 md: mx-4 opacity-90 transition-opacity hover:opacity-100 duration-100"
-            >
-              <figure>
-                <img
-                  className="w-full object-cover h-[150px] md:h-[250px]"
-                  src={urlFor(project?.image).url()}
-                  alt="car!"
-                />
-              </figure>
-              <div className="card-body h-96 pt-6 gap-0">
-                <div className="flex w-full justify-between">
-                  <h6 className="mb-6 text-xl font-light">{project?.title}</h6>
-
-                  <div className="flex space-x-3 pb-5 ">
-                    {project?.technologies?.map((tech) => {
-                      return (
-                        <div
-                          key={tech?._id}
-                          className="tooltip"
-                          data-tip={tech.title}
-                        >
-                          <Image
-                            src={urlFor(tech?.image).url()}
-                            height={30}
-                            width={30}
-                            alt="Tech"
-                          />
-                        </div>
-                      );
-                    })}
-                  </div>
+      <div className="h-screen scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory p-3 items-center z-20">
+        {personalProjects.map((project) => (
+          <div className="group relative cursor-pointer items-center w-[400px] h-[320px] md:w-[450px] md:h-[350px] snap-center flex-shrink-0 mx-4 transition-opacity duration-100 items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-xl">
+            <img
+              className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+              src={urlFor(project?.image).url()}
+              alt=""
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h1 className="text-xl  text-white">{project?.title}</h1>
+              <div className="mb-3 mt-6  text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                <div className="flex space-x-3 pb-5 ">
+                  {project?.technologies?.map((tech) => {
+                    return (
+                      <div
+                        key={tech?._id}
+                        className="tooltip"
+                        data-tip={tech.title}
+                      >
+                        <Image
+                          src={urlFor(tech?.image).url()}
+                          height={30}
+                          width={30}
+                          alt="Tech"
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
-
-                <div className="card-actions mb-6 mx-auto">
-                  <ExternalLink href={project?.linkToBuild}>
-                    <button className="btn btn-primary btn-sm">
-                      View site
-                    </button>
-                  </ExternalLink>
-                  <ExternalLink href={project?.linkToCode}>
-                    <button className="btn btn-primary btn-sm">
-                      View code
-                    </button>
-                  </ExternalLink>
-                </div>
-
-                <ul className="ml-2 space-y-2 overflow-y-scroll text-base list-disc max-h-96 pr-2">
-                  {project?.points?.map((point, i) => (
-                    <div key={i}>
-                      <li className="text-center">{point}</li>
-                      <div className="divider my-1"></div>
-                    </div>
-                  ))}
-                </ul>
+              </div>
+              <div className="card-actions mb-6 mx-auto">
+                <ExternalLink href={project?.linkToBuild}>
+                  <button className="btn btn-primary btn-sm mr-2">
+                    View site
+                  </button>
+                </ExternalLink>
+                <ExternalLink href={project?.linkToCode}>
+                  <button className="btn btn-primary btn-sm">View code</button>
+                </ExternalLink>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
-
-      {/* <div className='w-full absolute top-[30%] bg-[#f7ab0a]/40 left-0 h-[300px] -skew-y-6' /> */}
-    </div>
+    </>
   );
 }
