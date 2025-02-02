8ce035f569f34293e3b85cd1580d1efbb966c524
JakeLAmbertWD <jakelambert1@icloud.com>
2025-02-02 08:55:27 +0000

    Reduce background circles on smal screens

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -21,8 +21,8 @@ export const BackgroundCircles = ({ backgroundColor }: Props) => {
       <div className="absolute border [#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#bbafaf] rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
       <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
-      <div className="absolute border border-[#f7ab0a]  rounded-full h-[650px] w-[650px] mt-52 animate-pulse opacity-20" />
-      <div className="absolute border border-[#333333]  rounded-full h-[800px] w-[800px] mt-52 " />
+      <div className="absolute border border-[#f7ab0a]  rounded-full h-[618px] w-[618px] sm:h-[650px] sm:w-[650px] mt-52 animate-pulse opacity-20" />
+      <div className="absolute border border-[#333333]  rounded-full h-[760px] w-[760px] sm:h-[800px] sm:w-[800px] mt-52 " />
     </motion.div>
   );
 };
